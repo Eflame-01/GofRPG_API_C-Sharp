6919ff9fecf6f50a289077472cf34d8cfc1703ea
Ese <eomene2@uic.edu>
2021-03-22 20:19:47 -0500

    Update ItemDriver.cs

--- database/driver/ItemDriver.cs
+++ database/driver/ItemDriver.cs
@@ -6,6 +6,37 @@ namespace GofRPG_API
 {
     public class ItemDriver : DatabaseDriver
     {
-        
+        public Item GetItem(String name)
+        {
+            Item item = null;
+
+            if(MySqlConnection.State.ToString().Equals("Closed"))
+            {
+                InitDriver();
+            }
+
+            SqlQuery = "SELECT * FROM item WHERE item_name = '" + name + "'";
+            MySqlCommand = new MySqlCommand(SqlQuery, MySqlConnection);
+            MySqlDataReader = MySqlCommand.ExecuteReader();
+
+            if(MySqlDataReader.Read())
+            {
+                String itemName = MySqlDataReader.GetString(0);
+                String itemDescription = MySqlDataReader.GetString(1);
+                String itemType = MySqlDataReader.GetString(2);
+                int itemLevel = MySqlDataReader.GetInt32(3);
+
+                
+            }
+
+            return item;
+        }
+
+        private Item MakeItem(String name, String description, String type, int level)
+        {
+            Item item = null;
+
+            return item;
+        }
     }
 }
