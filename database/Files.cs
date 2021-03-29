d414ab695f6f961d293d201f94375a48e8672951
Ese <eomene2@uic.edu>
2021-03-29 09:28:46 -0500

    Added comments in Files.cs

--- database/Files.cs
+++ database/Files.cs
@@ -8,24 +8,25 @@ namespace GofRPG_API
     class Files
     {   
         //Data members
-        private String _encryptedFileName = "sql_encrypted_data.txt";
-        private String _fileName = "sql_connection_string.txt";
-        private Boolean _isEncrypted = false;
-        private byte[] _encrypted;
-        private byte[] _decrypted;
+        private String _encryptedFileName = "sql_encrypted_data.txt";   //actual data that we will use (in string value)
+        private String _fileName = "sql_connection_string.txt";         //name of sql encrypted data incase original is lost (in byte value)
+        private Boolean _isEncrypted = false;                           //checks if the file is encrypted
+        private byte[] _encrypted;                                      //value of the encrypted bytes
+        private byte[] _decrypted;                                      //value of the decrypted bytes
 
         public String DecryptFile()
         {
             //Find the file, and decrypt it if it is encrypted.
             if(File.Exists(_encryptedFileName))
             {
+                //if the encrypted file exists, get the bytes and delete the file
                 _encrypted = File.ReadAllBytes(_encryptedFileName);
                 _isEncrypted = true;
                 File.Delete(_encryptedFileName);
             }
             if( _isEncrypted)
             {
-                //decrypt file
+                //decrypt the bytes you get from the encrypted bytes if the file is encrypted
                 CspParameters cspParameters = new CspParameters();
                 cspParameters.KeyContainerName = "Container";
                 using(var rsa = new RSACryptoServiceProvider(2048, cspParameters))
@@ -58,6 +59,7 @@ namespace GofRPG_API
                     _encrypted = rsa.Encrypt(plain, true);
                 }
 
+                //write file into original file name and back up file name
                 File.WriteAllText(_fileName, BitConverter.ToString(_encrypted).Replace("-", ""));
                 File.WriteAllBytes(_encryptedFileName, _encrypted);
             }
@@ -68,6 +70,5 @@ namespace GofRPG_API
             //Return true if it is sucesscully encrypted. False if not.
             return true;
         }
-
     }
 }
