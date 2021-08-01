b09e404e145458812addea1479f809cb5009092c
Ese <eomene2@uic.edu>
2021-08-01 15:45:49 -0500

    Update MixedMartialArtist.cs
    updated the base stat totals for some of the stats

--- archetype/MixedMartialArtist.cs
+++ archetype/MixedMartialArtist.cs
@@ -9,7 +9,7 @@ namespace GofRPG_API
             ArchetypeID = ArchetypeID.MIXED_MARTIAL_ARTIST;
             MainArchetypeName = "BRAWLER";
             ArchetypeName = "MIXED MARTIAL ARTIST";
-            CharacterBaseStat = new BaseStat(6, 5, 2, 7, 4);
+            CharacterBaseStat = new BaseStat(6, 5, 3, 7, 4);
             LevelUpBoost1 = new BaseStat(3, 7, 5, 4, 6);
             LevelUpBoost2 = new BaseStat(4, 6, 5, 3, 7);
             LevelUpBoost3 = new BaseStat(5, 5, 5, 5, 5);
