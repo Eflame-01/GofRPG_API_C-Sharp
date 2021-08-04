739810cc3617beb914a21d7384a8cf7d6a182e02
Ese <eomene2@uic.edu>
2021-08-04 08:26:45 -0500

    Update PriorityMove.cs
    - added method that adjusts the priority level based on the information in the constuctor.

--- move/PriorityMove.cs
+++ move/PriorityMove.cs
@@ -7,6 +7,34 @@ namespace GofRPG_API
         public double FlinchProbability {get; set;}
         public double SuccessionRate {get; set;}
         public double OriginalFlinchProbability {get; set;}
+
+        public PriorityMove(Move secondaryMove, String name, String description, double accuracy, String target, int level, int energyPoints, int maxEnergyPoints, double powerPercent, int priorityLevel)
+        {
+            SecondaryMove = secondaryMove;
+            MoveName = name;
+            MoveDescription = description;
+            PrimaryMoveAccuracy = accuracy;
+            MoveTarget = target;
+            MoveLevel = level;
+            MoveEnergyPoints = energyPoints;
+            MoveMaxEnergyPoints = maxEnergyPoints;
+            MovePowerPercent = powerPercent;
+            AdjustPriorityMove(priorityLevel);
+        }
+
+        public PriorityMove(String name, String description, double accuracy, String target, int level, int energyPoints, int maxEnergyPoints, double powerPercent, int priorityLevel)
+        {
+            MoveName = name;
+            MoveDescription = description;
+            PrimaryMoveAccuracy = accuracy;
+            MoveTarget = target;
+            MoveLevel = level;
+            MoveEnergyPoints = energyPoints;
+            MoveMaxEnergyPoints = maxEnergyPoints;
+            MovePowerPercent = powerPercent;
+            AdjustPriorityMove(priorityLevel);
+        }
+
         public override void PerformMove(Character user, Character target)
         {
             if(user == null || target == null)
@@ -29,6 +57,37 @@ namespace GofRPG_API
         {
             Random rand = new Random();
             return (rand.NextDouble() <= FlinchProbability);
+        }
+
+        private void AdjustPriorityMove(int priorityLevel)
+        {
+            switch(priorityLevel)
+            {
+                case 1:
+                FlinchProbability = 1.0;
+                SuccessionRate = 0.21;
+                OriginalFlinchProbability = 1.0;
+                MoveType = "PRIORITY ONE";
+                break;
+                case 2:
+                FlinchProbability = 0.21;
+                SuccessionRate = 0.42;
+                OriginalFlinchProbability = 0.21;
+                MoveType = "PRIORITY TWO";
+                break;
+                case 3:
+                FlinchProbability = 0.0441;
+                SuccessionRate = 1.0;
+                OriginalFlinchProbability = 0.0441;
+                MoveType = "PRIORITY THREE";
+                break;
+                default:
+                FlinchProbability = 1.0;
+                SuccessionRate = 0.21;
+                OriginalFlinchProbability = 1.0;
+                MoveType = "PRIORITY ONE";
+                break;
+            }
         }
     }
 }
