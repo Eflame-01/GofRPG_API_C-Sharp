0104e72b84938dabad904655900381e9fee31812
Ese <eomene2@uic.edu>
2021-08-05 09:19:32 -0500

    Driver relocation
    deleting driver classes and adding them into character driver

--- database/driver/CharacterDriver.cs
+++ database/driver/CharacterDriver.cs
@@ -37,4 +37,70 @@ namespace GofRPG_API
             return npc;
         }
     }
+    class BaseStatDriver : DatabaseDriver
+    {
+        public BaseStat GetBaseStat(String characterID)
+        {
+            BaseStat baseStat = null;
+
+            if(MySqlConnection.State.Equals("Closed"))
+            {
+                InitDriver();
+            }
+
+            SqlQuery = "SELECT * FROM base_stat WHERE character_id = '" + characterID + "'";
+            MySqlCommand = new MySqlCommand(SqlQuery, MySqlConnection);
+            MySqlDataReader = MySqlCommand.ExecuteReader();
+
+            if(MySqlDataReader.Read())
+            {
+                int hp = MySqlDataReader.GetInt32(1);
+                int atk = MySqlDataReader.GetInt32(2);
+                int def = MySqlDataReader.GetInt32(3);
+                int eva = MySqlDataReader.GetInt32(4);
+                int spd = MySqlDataReader.GetInt32(5);
+
+                baseStat = new BaseStat(atk, def, eva, spd, hp);
+            }
+
+            return baseStat;
+        }
+    }
+
+    class MoveSetDriver : DatabaseDriver
+    {
+        private MoveDriver _moveDriver = new MoveDriver();
+        public MoveSet GetMoveSet(String characterID)
+        {
+            MoveSet moveSet = new MoveSet();
+            
+            if(MySqlConnection.State.Equals("Closed"))
+            {
+                InitDriver();
+            }
+
+            SqlQuery = "SELECT * FROM move_set WHERE character_id = '" + characterID + "'";
+            MySqlCommand = new MySqlCommand(SqlQuery, MySqlConnection);
+            MySqlDataReader = MySqlCommand.ExecuteReader();
+
+            if(MySqlDataReader.Read())
+            {
+                String moveOneName = MySqlDataReader.GetString(1);
+                String moveTwoName = MySqlDataReader.GetString(2);
+                String moveThreeName = MySqlDataReader.GetString(3);
+                String moveFourName = MySqlDataReader.GetString(4);
+
+                Move moveOne = _moveDriver.GetMove(moveOneName);
+                Move moveTwo = _moveDriver.GetMove(moveTwoName);
+                Move moveThree = _moveDriver.GetMove(moveThreeName);
+                Move moveFour = _moveDriver.GetMove(moveFourName);
+
+                moveSet.AddMove(moveOne);
+                moveSet.AddMove(moveTwo);
+                moveSet.AddMove(moveThree);
+                moveSet.AddMove(moveFour);
+            }
+            return moveSet;
+        }
+    }
 }
