1d12ccf72fdc123b0b7e7bc410118d25b3e7bc29
Ese <eomene2@uic.edu>
2021-08-10 08:38:54 -0500

    Create ItemID.cs

---
+++ item/ItemID.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace GofRPG_API
+{
+    public enum ItemID
+    {
+        FOOD,
+        HEALING,
+        KEY,
+        MEDICAL,
+        PRIORITY,
+        STAT_CHANGING
+    }
+}
